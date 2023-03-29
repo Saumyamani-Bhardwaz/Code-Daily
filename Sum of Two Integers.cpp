241e793160962df183abb53a68c4dab8f7fd7ed1
Saumyamani Bhardwaz <83209337+Saumyamani-Bhardwaz@users.noreply.github.com>
2023-03-30 00:30:08 +0530

    Sum of Two Integers

---
+++ Sum of Two Integers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getSum(int a, int b) 
+  {
+        while(b!=0)
+        {
+            unsigned carry=a&b;
+            a=a^b;
+            b=carry<<1;
+        }
+        return a;
+    }
+      
+       
+};
