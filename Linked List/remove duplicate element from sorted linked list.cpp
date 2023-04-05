4784eca221f61a1737ef4dcce9bea26a5ac9b1bb
Saumyamani Bhardwaz <83209337+Saumyamani-Bhardwaz@users.noreply.github.com>
2023-04-05 20:08:25 +0530

    remove duplicate element from sorted linked list.cpp
    remove duplicate element from sorted linked list (geeks for geeks)

---
+++ Linked List/remove duplicate element from sorted linked list.cpp
@@ -0,0 +1,16 @@
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+//Function to remove duplicates from sorted linked list.
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+        
+}
