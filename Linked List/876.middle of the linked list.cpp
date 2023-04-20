8cb2591c32b93b4505377cc6512bbbf067796c1c
Saumyamani Bhardwaz <83209337+Saumyamani-Bhardwaz@users.noreply.github.com>
2023-04-20 21:19:13 +0530

    Update 876.middle of the linked list.cpp

--- Linked List/876.middle of the linked list.cpp
+++ Linked List/876.middle of the linked list.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        
      // if(head==0 ) return 0;
       //if(head->next==0) return head;
+        
       ListNode* fast=head;
       ListNode* slow=head;
       while(fast!=0 &&fast->next!= 0 ){
