73b0d0fbefa43ffa96601e77805bc77d3b88b286
Saumyamani Bhardwaz <83209337+Saumyamani-Bhardwaz@users.noreply.github.com>
2023-06-07 23:06:59 +0530

    Create binaryTree (In-Order).cpp

---
+++ Trees/binaryTree (In-Order).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string customSortString(string order, string str) {
+        map<char, int>mp;
+        string ans = "";
+
+        for(auto x: str)
+            mp[x]++;
+        for(auto x: order){
+            if(mp.find(x)!=mp.end()){
+                auto temp = mp.find(x);
+                int count = temp->second;
+                // c-> 5  || ccccc
+                string s(count, x);
+                ans+=s;
+                mp.erase(x);
+            }
+        }
+
+        for(auto x: mp){
+            string s(x.second, x.first);
+            ans+=s;
+        }
+        return ans;
+    }
+};
